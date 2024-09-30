33fd73a74518e47759ac55e54e0e89a9e86dce68
iboxz <hadinezhad1383@gmail.com>
2024-09-30 04:27:25 +0330

    Main version (1.0v)

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-// Install 'ws' package using npm before running this code: npm install ws
 const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on("connection", (ws) => {
   ws.on("message", (message) => {
-    // Broadcast the message to all connected clients
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(message);
